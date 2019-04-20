6480386200dcb50e6013c69166d459748ac4cc67
unknown <unknown@example.com>
2019-04-20 22:28:39 +0800

    -style change some style

--- pages/port/port.js
+++ pages/port/port.js
@@ -9,15 +9,18 @@ Page({
     buildingData: [
       ['生活西区', '生活东区'], ['西一', '西二', '西三', '西四', '西五', '西六', '西七', '西八','西九','西十','西十一','西十二','西十三','西十四']
     ],
+    building: '请选择楼栋',
     bedIndex: 0,
-    bedData: ['一号床', '二号床', '三号床', '四号床']
+    bedData: ['一号床', '二号床', '三号床', '四号床'],
+    bed: '请选择床号'
   },
   /**
    * 床号改变函数
    */
   bedChange: function (e) {
     this.setData({
-      bedIndex: e.detail.value
+      bedIndex: e.detail.value,
+      bed: this.data.bedData[e.detail.value]
     })
   },
   /**
@@ -42,8 +45,10 @@ Page({
    */
   buildingChange: function(e) {
     console.log(e.detail.value)
+
     this.setData({
-      buildingIndex: e.detail.value
+      buildingIndex: e.detail.value,
+      building: this.data.buildingData[0][e.detail.value[0]] + '，' + this.data.buildingData[1][e.detail.value[1]]
     })
   },
   /**
