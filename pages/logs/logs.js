8747f3cd449aa508e6baf24206c7874570cf3d5e
unknown <unknown@example.com>
2019-05-08 22:19:56 +0800

    -feat add markdown plugin

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,30 @@
 //logs.js
-const util = require('../../utils/util.js')
+const app = getApp();
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    article: {}
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
+    let _this = this;
+    wx.request({
+      url: 'http://ed1son.cn:8001/doc/QG.md',
+      success: (res) => {
+        let articleData = app.towxml.toJson(res.data, 'markdown');
+        console.log(articleData);
+        articleData = app.towxml.initData(articleData, {
+          base: 'http://ed1son.cn:8001',
+          app: _this
+        });
+
+        // articleData.theme = 'light';
+
+
+        _this.setData({
+          article: articleData
+        })
+      }
     })
   }
 })
