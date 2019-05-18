b23b8ce4dfa032616fea87a62d9cd5c64bea7ab5
unknown <unknown@example.com>
2019-05-18 22:39:12 +0800

    -feat add blog flow

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,30 +1,15 @@
 //logs.js
-const app = getApp();
+const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: [],
-    article: {}
+    logs: []
   },
   onLoad: function () {
-    let _this = this;
-    wx.request({
-      url: 'http://ed1son.cn:8001/doc/QG.md',
-      success: (res) => {
-        let articleData = app.towxml.toJson(res.data, 'markdown');
-        console.log(articleData);
-        articleData = app.towxml.initData(articleData, {
-          base: 'http://ed1son.cn:8001',
-          app: _this
-        });
-
-        // articleData.theme = 'light';
-
-
-        _this.setData({
-          article: articleData
-        })
-      }
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
     })
   }
 })
