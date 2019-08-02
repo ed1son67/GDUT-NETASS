6f2160d0ea82ef434aca981f1647d011c1f58d6d
unknown <unknown@example.com>
2019-08-02 21:44:34 +0800

    feat:
    重写了文章页面的样式

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -10,9 +10,33 @@ Page({
       title: '',
       content: '',
       footer: ''
-    }]
+    }],
+    isActive: 0,
+    animationData: {},
+    
+  },
+  setActive: function(e) {
+    // 获取当前点击的index
+    var index = e.target.dataset.index;
+    // 初始化动画数据
+    var animation = wx.createAnimation({
+      duration: 300,
+      timingFunction: 'ease-out',
+      delay: 0
+    })
+    // 距离左边位置
+    animation.left((index * 50) + '%').step()
+    // 设置动画
+    this.setData({
+      animationData: animation.export()
+    })
+    // 设置对应class
+    this.setData({
+      isActive: index
+    })
   },
   onPullDownRefresh: function () {
+    wx.showNavigationBarLoading();
     this.queryAllBlog();
   },
   /**
@@ -35,14 +59,21 @@ Page({
         console.log(res.data);
         // 停止刷新
         wx.stopPullDownRefresh();
-        
+        wx.hideNavigationBarLoading();
         this.setData({
           articleData: res.data
         })
       }
     })
   },
+  
   onLoad: function () {
     this.queryAllBlog();
+    
+
+  },
+  onReachBottom: function() {
+    console.log(123);
+
   }
 })
