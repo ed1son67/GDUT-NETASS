07cb913953c16f3bacb486b00b672a168061a3c9
ed1son67 <1215373008@qq.com>
2020-09-09 20:31:05 +0800

    ui：修改首页展示信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,27 @@ const app = getApp()
 Page({
   data: {
   },
+  copyWebSite: function (e) {
+    const text = e.currentTarget.dataset.copy;
+    wx.showModal({
+      title: '点击确认复制文字',
+      content: text,
+      confirmText: '确认',
+      showCancel: false,
+      success: function () {
+        wx.setClipboardData({
+          data: text,
+          success(res) {
+            wx.getClipboardData({
+              success(res) {
+                console.log(res.data) // data
+              }
+            })
+          }
+        })
+      }
+    });
+  },
   onShareAppMessage: function () {
     return {
       title: "欢迎使用工大网络助手",
